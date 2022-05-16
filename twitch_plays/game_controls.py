4ffa38fbc15ba65b0261899982773fa540d5359e
Cole Gibbs <colegibbs0@gmail.com>
2022-05-16 11:07:29 -0700

    game controls function

---
+++ twitch_plays/game_controls.py
@@ -0,0 +1,26 @@
+import pyautogui
+
+def Controls(message):
+  while True:
+    message = message.lower()
+    if message == "up":
+      pyautogui.keyDown("up")
+      pyautogui.keyUp("up")
+    elif message == "down":
+      pyautogui.keyDown("down")
+      pyautogui.keyUp("down")
+    elif message == "left":
+      pyautogui.keyDown("left")
+      pyautogui.keyUp("left")
+    elif message == "right":
+      pyautogui.keyDown("right")
+      pyautogui.keyUp("right")
+    elif message == "a":
+      pyautogui.keyDown("s")
+      pyautogui.keyUp("s")
+    elif message == "b":
+      pyautogui.keyDown("a")
+      pyautogui.keyUp("a")
+    elif message == "start":
+      pyautogui.keyDown("enter")
+      pyautogui.keyUp("enter")
--- twitch_plays/twitch_plays.py
+++ twitch_plays/twitch_plays.py
@@ -1,6 +1,4 @@
 from dotenv import load_dotenv
 
 
-
-
 load_dotenv()
